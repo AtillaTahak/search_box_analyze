df95d4c38e6d528d606f5f71b8e85f832a251a05
atillatahak <atillatahaa@gmail.com>
2022-12-07 00:04:43 +0300

    added search form

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,10 @@ import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import SearchController from "controllers/search_controller"
 eagerLoadControllersFrom("controllers", application)
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+application.register("search-form", SearchController)
